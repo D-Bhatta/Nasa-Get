de03638956ff8e619d12c9dc3f431a7cbe5050b2
D-Bhatta <dbhatta1232@gmail.com>
2020-10-10 13:46:10 -0400

    :loud_sound: Add logging to app
    - Add logging to app
    - Log stuff in `homepage/view.py`

--- nasa_get/homepage/views.py
+++ nasa_get/homepage/views.py
@@ -1,7 +1,21 @@
+import logging
+import logging.config
+from json import load as jload
+from pathlib import Path
+
 from django.shortcuts import render
+
+# Configure logger lg with config for appLogger from config.json["logging"]
+CONFIG_DIR = Path(__file__).resolve().parent.parent.parent
+with open(CONFIG_DIR / "config.json", "r") as f:
+    config = jload(f)
+    logging.config.dictConfig(config["logging"])
+lg = logging.getLogger("appLogger")
+# lg.debug("This is a debug message")
 
 # Create your views here.
 
 
 def homepage(request):
+    lg.debug("Rendering homepage")
     return render(request, "home.html", {})
---
+++ notes/Notes_checklist_issue_4.md
@@ -0,0 +1,8 @@
+# Add Logging to app
+
+Add logging to views.py of the homepage app.
+
+- Get logger
+- Log something
+- refactor locally
+- Deploy
