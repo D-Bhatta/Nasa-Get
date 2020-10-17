f22715e2f2d83d46b4032d7ecb6f7436a17fc8c2
D-Bhatta <dbhatta1232@gmail.com>
2020-10-17 14:52:12 -0400

    Create models to store info about APIs
    - Create a model called `APIInfo`
    - Store API name, link, a picture of API result

--- nasa_get/view_api/models.py
+++ nasa_get/view_api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+# Model APIInfo: stores info about APIs
+class APIInfo(models.Model):
+    r"""Model to store API name, link, a picture of API result"""
+    name = models.TextField()
+    link = models.URLField()
+    image = models.FilePathField(path="/img")
