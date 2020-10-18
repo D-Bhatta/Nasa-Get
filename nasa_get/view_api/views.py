f6d2afcdeab9eb675e892c2741e3d045f451c891
D-Bhatta <dbhatta1232@gmail.com>
2020-10-18 06:54:49 -0400

    Add api_index view

--- nasa_get/view_api/views.py
+++ nasa_get/view_api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from view_api.models import APIInfo
 
 # Create your views here.
+
+
+def api_index(request):
+    # Get all APIInfo objects
+    apis = APIInfo.objects.all()  # pylint: disable="no-member"
+
+    # Create context dict
+    context = {"apis": apis}
+
+    # Render the APIs
+    return render(request, "api_index.html", context)
