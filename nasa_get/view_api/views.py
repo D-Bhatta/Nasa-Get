f9c6c90007be1826c524c54621f2a06f3fc59fa3
D-Bhatta <dbhatta1232@gmail.com>
2020-10-19 13:02:04 -0400

    Add logging to `views.py`

--- nasa_get/view_api/views.py
+++ nasa_get/view_api/views.py
@@ -1,5 +1,19 @@
+import json
+import logging
+import logging.config
+from json import load as jload
+from pathlib import Path
+
 from django.shortcuts import render
 from view_api.models import APIInfo
+
+# Configure logger lg with config for appLogger from config.json["logging"]
+CONFIG_DIR = Path(__file__).resolve().parent.parent.parent
+with open(CONFIG_DIR / "config.json", "r") as f:
+    config = jload(f)
+    logging.config.dictConfig(config["logging"])
+lg = logging.getLogger("appLogger")
+# lg.debug("This is a debug message")
 
 # Create your views here.
 
