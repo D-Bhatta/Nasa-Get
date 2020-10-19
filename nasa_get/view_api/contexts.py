b7da5c7e8dc9fadd64aad2f90d34af348d862c59
D-Bhatta <dbhatta1232@gmail.com>
2020-10-19 13:19:46 -0400

    Flesh out `ContextBuilder` class

--- nasa_get/view_api/contexts.py
+++ nasa_get/view_api/contexts.py
@@ -5,7 +5,7 @@ from json import load as jload
 from pathlib import Path
 
 from apis import get_api_result
-from utils import get_test_api_key
+from utils import get_test_api_key  # pylint: disable=import-error
 
 # Configure logger lg with config for appLogger from config.json["logging"]
 CONFIG_DIR = Path(__file__).resolve().parent.parent.parent
@@ -17,13 +17,53 @@ lg = logging.getLogger("appLogger")
 
 
 class ContextBuilder:
-    def __init__(self):
-        self.key = get_test_api_key()
-        self.provider = "Nasa"
+    def __init__(self, key: str, provider: str):
+        self.key = key
+        self.provider = provider
+        self.api_functions = {
+            "APOD": self.apod_context,
+            "EPIC": self.epic_context,
+            "DONKI": self.donki_notifications_context,
+            "MRP": self.mrp_context,
+        }
         self.name = ""
 
+    def build_context(self, name: str):
+        r"""Returns the context of a selected API result.
+
+        This method returns the context of a selected API result passed to it by
+        the `name` parameter.
+
+        Parameters
+        ----------
+        name : str
+            Name of the API.
+
+        Returns
+        -------
+        result : dict
+            A dict object that is the context of a selected API result.
+
+        Notes
+        -----
+        Only pass names supported by this class
+
+        Examples
+        --------
+        These are written in doctest format, and should illustrate how to
+        use the function.
+
+        >>> name = "APOD"
+        >>> result = nasa.query_api(name)
+        """
+        self.name = name
+        try:
+            func = self.api_functions[name]
+        except KeyError:
+            lg.error("Unknown API: This API is not supported")
+        return func()
+
     def apod_context(self):
-        self.name = "APOD"
         result = get_api_result(
             provider=self.provider, name=self.name, key=self.key
         )
@@ -50,10 +90,22 @@ class ContextBuilder:
 
         return context
 
+    def epic_context(self):
+        pass
+
+    def donki_notifications_context(self):
+        pass
+
+    def mrp_context(self):
+        pass
+
 
 def test_context():
-    context_builder = ContextBuilder()
-    context = context_builder.apod_context()
+    key = get_test_api_key()
+    provider = "Nasa"
+    name = "APOD"
+    context_builder = ContextBuilder(key, provider)
+    context = context_builder.build_context(name)
     lg.info(context)
 
 
